ac88acf5f20fa23fd052131bc0373d2838e6215f
Robbie <robbie331@>
2024-07-24 21:06:50 +0900

    1-3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -17,5 +17,30 @@ public class App {
         System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char operator = sc.next().charAt(0);
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+        int result = 0;
+        switch (operator) {
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if(num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("올바른 값을 입력하세요");
+                break;
+        }
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+        System.out.println("결과: " + result);
     }
 }
