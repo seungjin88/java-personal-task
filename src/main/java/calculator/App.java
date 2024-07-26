c9018bdc9d670fc1a40cb53f3bc1e56255d33130
Robbie <robbie331@>
2024-07-26 14:37:55 +0900

    1-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+
+        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
+        int[] intArray = new int[10]; //10개를 저장할 수 있는 배열 선언
+        int index = 0; // 변수 선언
         Scanner sc = new Scanner(System.in);
 
-        while (true) { // 혹은 for( ; ; ) 초깃값, 조건식, 변화식을 모두 생략하면 무한 루프
+        while (true) { // 혹은 for( ; ; ) 초깃값, 조건식, 증감식을 모두 생략하면 무한 루프
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
@@ -45,6 +50,11 @@ public class App {
             /* 반복문 사용 해서 연산을 반복 */
 
             System.out.println("결과: " + result);
+            /* 연산의 결과를 배열에 저장합니다. */
+            /* index를 증가 시킵니다. */
+            intArray[index] = result; // 순번 지정, [index] 없으면 어디 넣을지 알 수 없음, 배열[idx] = int,double,long등, 배열 = 다른배열
+                                      // 배열 없이 적을 경우 intArray = {1,2,3} 이런 식으로 작성
+            index++;
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.next();
